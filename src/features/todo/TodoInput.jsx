7b13ec7046fb105cc87b69bc70d87a0e3702d5b1
aryan9910 <aryanj9910@gmail.com>
2025-06-21 00:30:42 +0530

    changes

--- src/features/todo/TodoInput.jsx
+++ src/features/todo/TodoInput.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTodo } from './todoSlice';
-
-const [text, setText] = useState('');
-const dispatch= useDispatch();
-
-const handleSubmit= (e) => {
-  e.preventDefault();
-  const trimmedtext=text.trim();
-  if(!trimmedtext)
-  {
-    return;
-  }
-  dispatch(addTodo(trimmedtext));
-  setText('');
-
-}
+import { addTodo } from "./todoSlice";
 const TodoInput = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedtext = text.trim();
+    if (!trimmedtext) {
+      return;
+    }
+    dispatch(addTodo(trimmedtext));
+    setText("");
+  };
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
   return (
-   <form onSubmit={handleSubmit} style={{marginBottom:'1rem'}}>
-    <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder='Add a todo' style={{padding:'8px', fontSize:'16px'}}/>
-    <button type='submit' style={{marginLeft:'8px', padding:'8px'}}>
-    Add
-    </button>
-    
-   </form>
-  )
-}
+    <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Add a todo" style={{ padding: "8px", fontSize: "16px" }} />
+      <button type="submit" style={{ marginLeft: "8px", padding: "8px" }}>
+        Add
+      </button>
+    </form>
+  );
+};
 
-export default TodoInput
+export default TodoInput;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./App/store.js";
 
-createRoot(document.getElementById("root")).render(
+
+const root=createRoot(document.getElementById("root"));
+
+root.render(
   <StrictMode>
     <Provider store={store}>
       <App />
